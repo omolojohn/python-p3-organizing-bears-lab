c9dfc5113b0f9a9adbb68aaf2a048c4e40755c1e
omolo-j <omoloj135@gmail.com>
2024-06-01 15:27:05 +0300

    organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Calm', TRUE),
+(2, 'Rowdy', 8, 'M', 'Black', 'Aggressive', TRUE),
+(3, 'Tabitha', 5, 'F', 'Brown', 'Playful', TRUE),
+(4, 'Sergeant Brown', 12, 'M', 'Brown', 'Stern', TRUE),
+(5, 'Melissa', 7, 'F', 'Black', 'Gentle', TRUE),
+(6, 'Grinch', 6, 'M', 'Brown', 'Grumpy', FALSE),
+(7, 'Wendy', 4, 'F', 'White', 'Curious', TRUE),
+(8, NULL, 3, 'F', 'Black', 'Shy', TRUE);
